279f0543cc8d97cfaf6ec10df814e3dd6889240e
Romain Warnan <romain.warnan@gmail.com>
2016-11-26 21:24:38 +0100

    View PDF/XLS

--- src/main/java/fr/insee/bar/view/ClientsPdfView.java
+++ src/main/java/fr/insee/bar/view/ClientsPdfView.java
@@ -1,6 +1,7 @@
 package fr.insee.bar.view;
 
 import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 import fr.insee.bar.model.Client;
@@ -10,6 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
+import java.util.concurrent.Callable;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ClientsPdfView extends AbstractPdfView{
 
@@ -19,5 +27,18 @@ public class ClientsPdfView extends AbstractPdfView{
         for (Client client : clients) {
             document.add(new Paragraph(String.format("%s — %s", client.getNom(), client.getEmail())));
         }
+
+        clients.stream()
+                .map(c -> c.getNom() + " — " + c.getEmail())
+                .map(Paragraph::new)
+                .forEach((element) -> {
+                    try {
+                        document.add(element);
+                    } catch (DocumentException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+
     }
 }
