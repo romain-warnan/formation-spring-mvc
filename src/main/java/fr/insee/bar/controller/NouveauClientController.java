fe53e63240887a264bdaa9da69da88b8b05e9cfa
Romain Warnan <romain.warnan@gmail.com>
2017-05-14 11:48:31 +0200

    <form:form> dans l'énoncé du TP5

--- src/main/java/fr/insee/bar/controller/NouveauClientController.java
+++ src/main/java/fr/insee/bar/controller/NouveauClientController.java
@@ -1,5 +1,7 @@
 package fr.insee.bar.controller;
 
+import fr.insee.bar.dao.ClientDao;
+import fr.insee.bar.model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,21 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import fr.insee.bar.dao.ClientDao;
-import fr.insee.bar.model.Client;
-import fr.insee.bar.model.Client.Titre;
-
 @Controller
 @RequestMapping("/client")
 public class NouveauClientController {
 
 	@Autowired
 	private ClientDao clientDao;
-
-	@ModelAttribute("titres")
-	private Titre[] titres() {
-		return Titre.values();
-	}
 
 	@GetMapping("/nouveau")
 	public String nouveauClient(Model model) {
