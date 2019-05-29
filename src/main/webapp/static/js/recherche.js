20e7d8f94a9b779baf13d4b44a0b2782c4fc0222
Romain Warnan <romain.warnan@insee.fr>
2019-05-29 09:26:04 +0200

    fixes #10 : passage à ES6

--- src/main/webapp/static/js/recherche.js
+++ src/main/webapp/static/js/recherche.js
@@ -1,87 +1,83 @@
-$(document).ready(function() {
+$(document).ready(() => {
 	
-	var context = this;
-	var $champ = $('#champ-recherche');
-	var $suggestions = $('#suggestions');
-	var $commande = $('#commande');
-	var $bouton = $('#bouton-commander');
-	var $prix = $('#prix');
+	const context = this;
+	const $champ = $('#champ-recherche')
+	const $suggestions = $('#suggestions')
+	const $commande = $('#commande')
+	const $bouton = $('#bouton-commander')
+	const $prix = $('#prix')
+	const $message = $('#message')
 	
-	var initialiser = function(context){
-		$suggestions.hide();
-		$champ.keyup(function(){
-			rechercher($champ.val());
-		});
-		$bouton.click(function() {
-			commander();
-		});
-	};
+	const initialiser = context => {
+		$suggestions.hide()
+		$champ.keyup(() => rechercher($champ.val()))
+		$bouton.click(commander)
+	}
 	
-	var rechercher = function(q){
+	const rechercher = q => {
 		// Récupérer la liste des cocktails correspondants à la recherche "q"
 		// Appeler la méthode "afficherSuggestions" avec cette liste
 	};
 	
-	var commander = function() {
+	const commander = () => {
 		// Récupérer la liste des cocktails sélectionnés avec la fonction "cocktailsChoisis"
 		// La poster vers le serveur
 		// Récupérer en retour le prix total de la commande
 		// Appeler la méthode "afficherPrix" avec cette valeur
 	};
 	
-	var cocktailsChoisis = function() {
-		var cocktails = [];
-		$('#commande li.hidden').each(function(index, item) {
-			cocktails.push({
-				id: $(item).text()
-			});
-		});
-		return cocktails;
-	};
+	const effacerErreur = () => {
+		$message.empty()
+		$message.removeClass("error")
+	}
 	
-	var afficherSuggestions = function(cocktails) {
+	const afficherErreur = message => {
+		$message.text(message)
+		$message.addClass("error")
+	}
+	
+	const afficherSuggestions = cocktails => {
 		if(cocktails.length > 0){
-			suggerer(cocktails);
-			$suggestions.show();
+			suggerer(cocktails)
+			$suggestions.show()
 		}
 		else {
-			$suggestions.hide();
+			$suggestions.hide()
 		}
-	};
+	}
 	
-	var afficherPrix = function(prix){
-		$prix.text(': ' + prix + ' €');
-	};
+	const afficherPrix = prix => $prix.text(': ' + prix + ' €')
 	
-	var suggerer = function(cocktails){
-		$suggestions.empty();
-		$.each(cocktails, function(index, cocktail) {
-			$suggestions.append($('<li>').append(cocktail.nom));
-			$suggestions.append($('<li class="hidden">').append(cocktail.id));
-		});
-		$suggestions.find('li').each(function(index, item) {
-			$item = $(item);
-			$item.click(function() {
-				var $libelle = $(this);
-				var $id = $libelle.next();
-				choisir($id.text(), $libelle.text());
-			});
+	const suggerer = cocktails => {
+		$suggestions.empty()
+		$.each(cocktails, (index, cocktail) => {
+			$suggestions.append($('<li>').append(cocktail.nom))
+			$suggestions.append($('<li class="hidden">').append(cocktail.id))
 		})
-	};
+		$suggestions.find('li').each((index, item) => {
+			$item = $(item)
+			$item.click(function() {
+				const $libelle = $(this)
+				const $id = $libelle.next()
+				choisir($id.text(), $libelle.text())
+			})
+		})
+	}
 	
-	var choisir = function(id, nom){
-		var $item = $('<li>').append(nom);
+	const choisir = (id, nom) => {
+		effacerErreur()
+		const $item = $('<li>').append(nom)
 		$item.click(function() {
-			$(this).next().remove();
-			$(this).remove();
-		});
-		$commande.append($item);
-		$commande.append($('<li class="hidden">').append(id));
-		$suggestions.empty();
-		$suggestions.hide();
-		$champ.val('');
-		$champ.focus();
-	};
+			$(this).next().remove()
+			$(this).remove()
+		})
+		$commande.append($item)
+		$commande.append($('<li class="hidden">').append(id))
+		$suggestions.empty()
+		$suggestions.hide()
+		$champ.val('')
+		$champ.focus()
+	}
 	
-	initialiser(context);
-});
+	initialiser(context)
+})
