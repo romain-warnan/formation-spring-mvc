d4a37ba0a00315e2801d9432487c4a1bf3733368
Romain Warnan <romain.warnan@insee.fr>
2019-05-29 09:26:04 +0200

    fixes #10 : passage à ES6

--- src/main/webapp/static/js/recherche.js
+++ src/main/webapp/static/js/recherche.js
@@ -1,105 +1,93 @@
-$(document).ready(function() {
+$(document).ready(() => {
 	
-	var context = this;
-	var $champ = $('#champ-recherche');
-	var $suggestions = $('#suggestions');
-	var $commande = $('#commande');
-	var $bouton = $('#bouton-commander');
-	var $prix = $('#prix');
-	var $message = $('#message');
+	const context = this;
+	const $champ = $('#champ-recherche')
+	const $suggestions = $('#suggestions')
+	const $commande = $('#commande')
+	const $bouton = $('#bouton-commander')
+	const $prix = $('#prix')
+	const $message = $('#message')
 	
-	var initialiser = function(context){
-		$suggestions.hide();
-		$champ.keyup(function(){
-			rechercher($champ.val());
-		});
-		$bouton.click(function() {
-			commander();
-		});
-	};
+	const initialiser = context => {
+		$suggestions.hide()
+		$champ.keyup(() => rechercher($champ.val()))
+		$bouton.click(commander)
+	}
 	
-	var rechercher = function(q){
+	const rechercher = q => {
 		$.ajax({
 			url: '/cocktails/recherche',
 			method: 'GET',
-			data: {'q': q}
-		}).done(function(cocktails) {
-			afficherSuggestions(cocktails);
-		});
-	};
+			data: {'q': q},
+		})
+		.done(cocktails => afficherSuggestions(cocktails))
+	}
 	
-	var commander = function() {
-		var cocktails = [];
-		$('#commande li.hidden').each(function(index, item) {
-			cocktails.push({id: $(item).text()});
-		});
+	const commander = () => {
+		const cocktails = [];
+		$('#commande li.hidden').each((index, item)  => cocktails.push({id: $(item).text()}))
 		$.ajax({
 			url: '/cocktails/commande',
 			method: 'POST',
 			contentType : 'application/json; charset=utf-8',
-			data: JSON.stringify(cocktails)
-		}).done(function(prix) {
-			afficherPrix(prix);
-		}).fail(function(response) {
-			afficherErreur(response.responseText);
-		});
-	};
+			data: JSON.stringify(cocktails),
+		})
+		.done(prix => afficherPrix(prix))
+		.fail(response => afficherErreur(response.responseText))
+	}
 	
-	var effacerErreur = function(){
-		$message.empty();
-		$message.removeClass("error");
-	};
+	const effacerErreur = () => {
+		$message.empty()
+		$message.removeClass("error")
+	}
 	
-	var afficherErreur = function(message) {
-		$message.text(message);
-		$message.addClass("error");
-	};
+	const afficherErreur = message => {
+		$message.text(message)
+		$message.addClass("error")
+	}
 	
-	var afficherSuggestions = function(cocktails) {
+	const afficherSuggestions = cocktails => {
 		if(cocktails.length > 0){
-			suggerer(cocktails);
-			$suggestions.show();
+			suggerer(cocktails)
+			$suggestions.show()
 		}
 		else {
-			$suggestions.hide();
+			$suggestions.hide()
 		}
-	};
+	}
 	
-	var afficherPrix = function(prix){
-		$prix.text(': ' + prix + ' €');
-	};
+	const afficherPrix = prix => $prix.text(': ' + prix + ' €')
 	
-	var suggerer = function(cocktails){
-		$suggestions.empty();
-		$.each(cocktails, function(index, cocktail) {
-			$suggestions.append($('<li>').append(cocktail.nom));
-			$suggestions.append($('<li class="hidden">').append(cocktail.id));
-		});
-		$suggestions.find('li').each(function(index, item) {
-			$item = $(item);
-			$item.click(function() {
-				var $libelle = $(this);
-				var $id = $libelle.next();
-				choisir($id.text(), $libelle.text());
-			});
+	const suggerer = cocktails => {
+		$suggestions.empty()
+		$.each(cocktails, (index, cocktail) => {
+			$suggestions.append($('<li>').append(cocktail.nom))
+			$suggestions.append($('<li class="hidden">').append(cocktail.id))
 		})
-	};
+		$suggestions.find('li').each((index, item) => {
+			$item = $(item)
+			$item.click(function() {
+				const $libelle = $(this)
+				const $id = $libelle.next()
+				choisir($id.text(), $libelle.text())
+			})
+		})
+	}
 	
-	var choisir = function(id, nom){
-		effacerErreur();
-		var $item = $('<li>').append(nom);
+	const choisir = (id, nom) => {
+		effacerErreur()
+		const $item = $('<li>').append(nom)
 		$item.click(function() {
-			$(this).next().remove();
-			$(this).remove();
-		});
-		$commande.append($item);
-		$commande.append($('<li class="hidden">').append(id));
-		$suggestions.empty();
-		$suggestions.hide();
-		$champ.val('');
-		$champ.focus();
-	};
+			$(this).next().remove()
+			$(this).remove()
+		})
+		$commande.append($item)
+		$commande.append($('<li class="hidden">').append(id))
+		$suggestions.empty()
+		$suggestions.hide()
+		$champ.val('')
+		$champ.focus()
+	}
 	
-	
-	initialiser(context);
-});
+	initialiser(context)
+})
